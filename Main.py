bc07a98f960606efb521effba51148b8d1dba3b3
MANOJ KUMAR <smk11500@gmail.com>
2021-05-03 22:39:53 +0530

    Update

--- Main.py
+++ Main.py
@@ -1,17 +1,21 @@
 """
 Date started   : 09 Apr 2021
-Authors        : MANOJ_KUMAR_S | SARAVANAN_M | HARI_PRASAD_J
+Developers     : MANOJ_KUMAR_S | SARAVANAN_M | HARI_PRASAD_J
 """
-from utils import *
+from PyQt5.QtWidgets import QFileDialog
+
 import sys
-from PyQt5 import QtCore, QtGui, QtWidgets
-from login_frontend import Ui_Dialog
+from PyQt5 import QtWidgets
+from login_frontend import LoginFrontend
+from utils import close_db
 
 app = QtWidgets.QApplication(sys.argv)
 app.setStyle("fusion")
 Dialog = QtWidgets.QDialog()
 
-ui = Ui_Dialog()
+ui = LoginFrontend()
 ui.setupUi(Dialog)
 Dialog.show()
-sys.exit(app.exec_())
+if not app.exec_():
+    close_db()
+    sys.exit()
--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ import numpy as np
 import datetime
 
 # connecting to the database
+import pandas as pd
+
 conn = sqlite3.connect("dataBase/MasterDB.db")
 cursor = conn.cursor()
 
@@ -13,6 +15,20 @@ def get_user_names():
     users = np.array(cursor.fetchall())
     return users.flatten().tolist()
 
+def get_students_register_number():
+    cursor.execute("SELECT rollno FROM student_details")
+    book_ids = np.array(cursor.fetchall())
+    return book_ids.flatten().tolist()
+
+def get_book_id():
+    cursor.execute("SELECT book_id FROM book_details")
+    roll_no = np.array(cursor.fetchall())
+    return roll_no.flatten().tolist()
+
+def get_faculty_roll_no():
+    cursor.execute("SELECT roll_number FROM faculty_details")
+    faculty_roll_no = np.array(cursor.fetchall())
+    return faculty_roll_no.flatten().tolist()
 
 def create_new_user(user_name, pass_word, namee, desig):
     # Creating the new user and adding that to DB
@@ -21,9 +37,9 @@ def create_new_user(user_name, pass_word, namee, desig):
     TOTAL_LIBRARIAN += 1
     cursor.execute(
         "INSERT INTO Librarian (id, username, password, name, designation) VALUES ({}, '{}', '{}', '{}', '{}')".format(
-                                                                                            TOTAL_LIBRARIAN,
-                                                                                            user_name, pass_word,
-                                                                                            namee, desig))
+            TOTAL_LIBRARIAN,
+            user_name, pass_word,
+            namee, desig))
     conn.commit()
 
 
@@ -45,15 +61,16 @@ def validate_user(user_name, pass_word):
         return False
 
 
-def add_book_2_library(book_id, title, author):
-    cursor.execute("INSERT INTO book_details VALUES ('{}', '{}', '{}')".format(book_id, title, author))
+def add_book_2_library(book_id, type, title, author, publisher):
+    cursor.execute("INSERT INTO book_details VALUES ('{}', '{}', '{}', '{}', '{}')".format(book_id, title.lower(),
+                                                                                           type, author.lower(), publisher.lower()))
     conn.commit()
     cursor.execute("SELECT titles FROM available_books")
     availe_books_title = np.array(cursor.fetchall()).flatten().tolist()
     if title in availe_books_title:
         cursor.execute(
-            "UPDATE available_books SET numbers = (SELECT numbers FROM available_books where titles='{}')+1".format(
-                title))
+            "UPDATE available_books SET numbers = (SELECT numbers FROM available_books where titles='{}')+1 WHERE titles='{}'".format(
+                title, title))
         conn.commit()
     else:
         cursor.execute("INSERT INTO available_books values ('{}', {})".format(title, 1))
@@ -104,11 +121,94 @@ def return_book(roll_no, book_id, curr_librarian):
     conn.commit()
 
 
-def add_student_2_db(roll_no, stu_name, stu_semester, stu_dept):
-    cursor.execute("INSERT INTO student_details VALUES ('{}', '{}', '{}', '{}')".format(roll_no, stu_name,
-                                                                                        stu_semester, stu_dept))
+def add_student_2_db(roll_no, stu_name, year, stu_semester, stu_dept):
+    cursor.execute("INSERT INTO student_details VALUES ('{}', '{}', '{}', '{}', '{}')".format(roll_no, stu_name, year,
+                                                                                              stu_semester, stu_dept))
     conn.commit()
 
+def add_faculty_2_db(roll_no, name, designation, department):
+    cursor.execute("INSERT INTO faculty_details VALUES ('{}', '{}', '{}', '{}')".format(roll_no, name,
+                                                                                        designation, department))
+    conn.commit()
+
+def gen_book_id_for_new_book():
+    cursor.execute("SELECT book_id FROM book_details")
+    xx = cursor.fetchall()
+    ids = len(np.array(xx).flatten()) + 1
+    return "CS" + str(ids)
+
+
+def add_by_excel(filepath, types=None):
+    if types!=None:
+        try:
+            data = pd.read_excel(filepath)
+        except:
+            return False
+        if types=="book":
+            columns = ["title", "type", "author", "publisher"]
+            data = data[columns]
+            book_ids = []
+            for i in range(len(data)):
+                row = (data.iloc[i]).values
+                gen_id = gen_book_id_for_new_book()
+                book_ids.append(gen_id)
+                add_book_2_library(book_id=gen_id, type=row[1], title=row[0],
+                                   author=row[2], publisher=row[-1])
+
+            data["book_id"] = np.array(book_ids)
+            new_path = "/".join(filepath.split("/")[:-1])
+            data.to_csv(new_path+"/added_books_with_GeneratedBooks_id.csv")
+
+        if types=="student":
+            columns = ["rollno", "name", "year", "semester", "department"]
+            data = data[columns]
+            for i in range(len(data)):
+                row = (data.iloc[i]).values
+                add_student_2_db(roll_no=row[0], stu_name=row[1], year=row[2],
+                                 stu_semester=row[3], stu_dept=row[4])
+
+        if types=="faculty":
+            columns=["roll_number", "name", "designation", "department"]
+            data = data[columns]
+            for i in range(len(data)):
+                row = (data.iloc[i]).values
+                add_faculty_2_db(row[0], row[1], row[2], row[3])
+        return True
+    else:
+        return False
+
+
+def get_available_book_by_title(title_substr):
+    cursor.execute("SELECT titles FROM available_books")
+    titles = np.array(cursor.fetchall()).flatten()
+    ls = []
+    for title in titles:
+        if title_substr.lower() in title:
+            cursor.execute("SELECT book_id, title, author, type FROM book_details where title='{}'".format(title))
+            ls += np.array(cursor.fetchall()).tolist()
+    return ls
+
+def get_available_book_by_type(typee):
+    cursor.execute("SELECT book_id, title, author, type FROM book_details where type='{}'".format(typee))
+    return np.array(cursor.fetchall()).tolist()
+
+
+def get_student_burrowed_details(reg_no):
+    cursor.execute("SELECT book_id, title_of_book, given_date, return_date WHERE rollno={} AND is_returned='no'".format(reg_no))
+    return np.array(cursor.fetchall()).tolist()
+
+def get_faculty_burrowed_details(roll_no):
+    cursor.execute("SELECT book_id, title_of_book, given_date, return_date WHERE rollno={} AND is_returned='no' AND type='Faculty'".format(roll_no))
+    return np.array(cursor.fetchall()).tolist()
+
+def get_stu_details(roll_no):
+    cursor.execute("SELECT name, year, department FROM student_details WHERE rollno='{}'".format(roll_no))
+    return np.array(cursor.fetchall()).squeeze().tolist()
+
+
+def get_faculty_details(roll_no):
+    cursor.execute("SELECT name, designation, department FROM faculty_details WHERE roll_number='{}".format(roll_no))
+    return np.array(cursor.fetchall()).squeeze().tolist()
 
 def close_db():
     conn.close()
